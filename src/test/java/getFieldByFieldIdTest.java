a75c2cc2c41c6678cdff908ad2e1f2a668168362
MABERG <maberg@equinor.com>
2023-03-14 14:31:57 +0100

    Add spaces

--- src/test/java/getFieldByFieldIdTest.java
+++ src/test/java/getFieldByFieldIdTest.java
@@ -11,7 +11,7 @@ public class getFieldByFieldIdTest {
     @Test
     public void getFieldByFieldIdTes() throws IOException, JSONException {
         String fieldId = "62215471";
-        String updateDate = "2021-12-01T09:32:09";
+        String updateDate = "2021-12-01T09:32:09       ";
         GetFieldByFieldIdApi getFieldByFieldIdApi = new GetFieldByFieldIdApi();
 
         JSONObject jsonObject = getFieldByFieldIdApi.getFieldByFieldId(fieldId);
