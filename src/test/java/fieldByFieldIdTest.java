8930602b3f76e41a581c27d3706c2c0c1d4eda63
asajj <adeelsheikh.sajjad@gmail.com>
2023-03-14 14:09:50 +0100

    Remove space

--- src/test/java/fieldByFieldIdTest.java
+++ src/test/java/fieldByFieldIdTest.java
@@ -7,16 +7,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
-public class getFieldByFieldIdTest {
-    JSONObject jsonObject = null;
-
+public class fieldByFieldIdTest {
     @Test
     public void getFieldByFieldIdTes() throws IOException, JSONException {
-        String fieldId = "62215471";
+        String fieldId = "622154712";
         String updateDate = "2021-12-01T09:32:09";
         GetFieldByFieldIdApi getFieldByFieldIdApi = new GetFieldByFieldIdApi();
 
-        jsonObject = getFieldByFieldIdApi.getFieldByFieldId(fieldId);
+        JSONObject jsonObject = getFieldByFieldIdApi.getFieldByFieldId(fieldId);
         System.out.println("The value UpdateDate is: " + FieldDetailsController.getFieldDetailsObject(jsonObject).getUpdateDate());
         Assert.assertEquals(FieldDetailsController.getFieldDetailsObject(jsonObject).getUpdateDate(), (updateDate));
 
