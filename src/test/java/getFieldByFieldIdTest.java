402dc2e3088c9b81d95c6d83b31ab432220515bc
asajj <adeelsheikh.sajjad@gmail.com>
2023-03-14 14:27:07 +0100

    Remove import and update JSONObject

--- src/test/java/getFieldByFieldIdTest.java
+++ src/test/java/getFieldByFieldIdTest.java
@@ -1,6 +1,4 @@
-
-import models.referencegradecatalog.GradeDetailsController;
-import no.equinor.test.wellcom.rest.GetFieldByFieldIdApi;
+import no.equinor.test.wellcom.rest.fields.GetFieldByFieldIdApi;
 import models.fields.FieldDetailsController;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,15 +7,14 @@ import org.junit.Test;
 
 import java.io.IOException;
 public class getFieldByFieldIdTest {
-    JSONObject jsonObject = null;
 
     @Test
     public void getFieldByFieldIdTes() throws IOException, JSONException {
         String fieldId = "62215471";
-        String updateDate = "2021-12-01T09:32:09  ";
+        String updateDate = "2021-12-01T09:32:09";
         GetFieldByFieldIdApi getFieldByFieldIdApi = new GetFieldByFieldIdApi();
 
-        jsonObject = getFieldByFieldIdApi.getFieldByFieldId(fieldId);
+        JSONObject jsonObject = getFieldByFieldIdApi.getFieldByFieldId(fieldId);
         System.out.println("The value UpdateDate is: " + FieldDetailsController.getFieldDetailsObject(jsonObject).getUpdateDate());
         Assert.assertEquals(FieldDetailsController.getFieldDetailsObject(jsonObject).getUpdateDate(), (updateDate));
 
